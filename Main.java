6c4d7e0ff59c7739dc493f4bce9447b9e4b933e8
Jain <vaibhav.jain@dxc.com>
2023-09-08 12:05:24 +0530

    second commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public static void main(String gg[])
 System.out.println("This is first Java Program");
 System.out.println("This is Second Line of Program");
 System.out.println(new Date());
+System.out.println("Adding this line for testing..");
 }
 }
