583887e6bda91e2f9ba6e51ae6b525a7f711fbf3
Jain <vaibhav.jain@dxc.com>
2023-09-08 11:50:48 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main
+{
+public static void main(String gg[])
+{
+System.out.println("This is first Java Program");
+System.out.println("This is Second Line of Program");
+System.out.println(new Date());
+}
+}
